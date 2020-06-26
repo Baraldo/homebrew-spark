eca835a8a456641d9ae325edb8dda5b8ef92f735
Luiz Felipe de Mesquita Baraldo <luiz.felipe.m.baraldo@hotmail.com>
2020-06-26 15:11:01 -0300

    Update apache-spark@2.4.5.rb

--- Formula/apache-spark@2.4.5.rb
+++ Formula/apache-spark@2.4.5.rb
@@ -1,7 +1,7 @@
 class ApacheSparkAT245 < Formula
     desc "Engine for large-scale data processing"
     homepage "https://spark.apache.org/"
-    url "https://www.apache.org/dyn/closer.lua/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
+    url "https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz"
     version "2.4.5"
     sha256 "020be52524e4df366eb974d41a6e18fcb6efcaba9a51632169e917c74267dd81"
     head "https://github.com/apache/spark.git"
